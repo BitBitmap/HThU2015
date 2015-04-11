29ca475c2313de224826006f7f3edbc1a9b92466
richgoldfeld <richgoldfeld@gmail.com>
2015-04-11 01:41:50 -0400

    Updated to correct SQL relation error.

--- database/halpme.sql
+++ database/halpme.sql
@@ -1,21 +1,26 @@
-/*
-Team ThetaUpsilon
-Created on 4/11/15
-Copyright (c) 2015    All rights reserved
-*/
+--Team ThetaUpsilon
+--Created on 4/11/15
+--Copyright (c) 2015    All rights reserved
 DROP DATABASE IF EXISTS HalpMe;
 CREATE DATABASE IF NOT EXISTS HalpMe;
 USE HalpMe;
-/*
-	Problem solving tutoring system similar to craigslist
-	SQL 
-*/
--- USER
+--	Problem solving tutoring system similar to craigslist
+
+-- MEMBER
 create table member(
 	netID varchar(30),
 	password  char(32),
 	PRIMARY KEY (netID)
 );
+
+-- CLASS
+create table classes(
+	course varchar(30),
+	department varchar(32),
+	description varchar (50),
+	PRIMARY KEY (course)
+);
+
 -- REQUEST
 create table request(
 	RID integer auto_increment,
@@ -25,13 +30,6 @@ create table request(
 	requestdatetime datetime,
 	netID varchar (30),
 	PRIMARY KEY (RID),
-    FOREIGN KEY (netID) REFERENCES member(netID).
+    FOREIGN KEY (netID) REFERENCES member(netID),
     FOREIGN KEY (course) REFERENCES classes(course)
-);
--- CLASS
-create table classes(
-	course varchar(30),
-	department varchar(32),
-	description varchar (50),
-	PRIMARY KEY (course)
 );
