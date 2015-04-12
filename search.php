8ab29350dd17250856cdee552ef4e8a948805005
richgoldfeld <richgoldfeld@gmail.com>
2015-04-11 23:07:14 -0400

    Fixed GIT ERROR with search

--- search.php
+++ search.php
@@ -62,7 +62,6 @@
     <div class="container">
 	    <form method = 'get' class="form-inline">
 			<div class="form-group">
-<<<<<<< Updated upstream
 				<label for="sel1">Select Department:</label>
         <?php
         $stmt = $mysqli->prepare("SELECT DISTINCT department from classes");
@@ -75,17 +74,8 @@
           }
           $stmt->close();
           ?>
-=======
-				<label for="sel1">Select Department:</label><br>
-				<select name='department' class="input-large" id="sel1">
-	        <option>Select All</option>
-					<option>1</option>
-					<option>2</option>
-					<option>3</option>
-					<option>4</option>
->>>>>>> Stashed changes
 	  			</select>
-			</div><br><br>
+			</div>
 			<button type="submit" class="btn btn-default">Submit</button>	
 	    </form>
 	    <br>
